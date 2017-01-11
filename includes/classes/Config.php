23e26325c4636b64f6988bff331fd8682bf3f62b
Ajay <ajay10mar96@gmail.com>
2017-01-11 21:36:35 +0530

    3

--- includes/classes/Config.php
+++ includes/classes/Config.php
@@ -1,10 +1,8 @@
 <?php
 
-defined("DB_SERVER")? null: define("DB_SERVER","localhost");
-defined("DB_USER")? null:define("DB_USER","root");
-defined("DB_PASS")? null:define("DB_PASS","1234");
-defined("DB_NAME")? null:define("DB_NAME","ecommerce");
+defined("DB_SERVER")? null: define("DB_SERVER","us-cdbr-iron-east-04.cleardb.net");
+defined("DB_USER")? null:define("DB_USER","b90db1c726b8df");
+defined("DB_PASS")? null:define("DB_PASS","8b114903");
+defined("DB_NAME")? null:define("DB_NAME","heroku_627773ec3fcaf1b");
 
-
-// mysql://b90db1c726b8df:8b114903@us-cdbr-iron-east-04.cleardb.net/heroku_627773ec3fcaf1b?reconnect=true
 ?>
